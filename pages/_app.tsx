7bb0a0566bb21f9da22afb06dffe480b8f30a654
Alberto De Agostini <albertodeagostini.dev@gmail.com>
2022-01-13 18:28:47 +0100

    add panelbear for analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
+import { usePanelbear } from "@panelbear/panelbear-nextjs";
 import "../styles/reset.css";
 import "../styles/theme.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Load Panelbear only once during the app lifecycle
+  usePanelbear("Ah30xQJSiNU", {
+    debug: process.env.NODE_ENV !== "production",
+  });
+
   return <Component {...pageProps} />;
 }
 
